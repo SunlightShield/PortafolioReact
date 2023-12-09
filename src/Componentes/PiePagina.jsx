0edb5e318610048f23ce500766fad7e1389e4712
SunlightShield <sespinozahenriquez@gmail.com>
2023-12-09 20:56:35 -0300

    fondo

--- src/Componentes/PiePagina.jsx
+++ src/Componentes/PiePagina.jsx
@@ -16,27 +16,22 @@ function PiePagina() {
                 <div class="footer-top bg-secondary-custom">
                     <div class="container">
                         <ul class="list-inline mb-0">
-                            <li class="list-inline-item mr-4">
+                            <li class="list-inline-item mr-5">
                                 <a class="link-primary" href="https://github.com/SunlightShield" title="freeCodeCamp" target="_blank" rel="noopener noreferrer">
                                     <img id='ImagenGit' src={git} alt="ImagenGit!!..." style={{ width: '100px', height: '100px' }} />
                                 </a>
                             </li>
-                            <li class="list-inline-item mr-4">
-                                <a class="link-primary" href="www.linkedin.com/in/sebastian-espinoza-henriquez" title="CodePen" target="_blank" rel="noopener noreferrer">
-                                    <img id='Linkedin' src={linkedn} alt="Linkedin!!..." style={{ width: '100px', height: '100px' }} />
+                            <li class="list-inline-item mr-5">
+                                <a class="link-primary" href="https://linkedin.com/in/sebastian-espinoza-henriquez" title="CodePen" target="_blank" rel="noopener noreferrer">
+                                    <img id='Linkedin' src={linkedn} alt="Linkedin!!..." style={{ width: '70px', height: '70px' }} />
                                 </a>
                             </li>
-                            {/* <li class="list-inline-item">
-                                <a class="link-primary" href="https://github.com/webmastervito" title="GitHub" target="_blank" rel="noopener noreferrer">
-                                    <i class="fa fa-github fa-2x anim-fourth-seq anim-fourth-seq-js"></i>
-                                </a>
-                            </li> */}
                         </ul>
                     </div>
                 </div>
                 <div class="footer-bottom bg-dark-custom">
                     <div class="container">
-                        <p style={{ color: "white" }}>Coded by Sebatian Espinoza 2023</p>
+                        <p style={{ color: "white" }}>developed by Sebatian Espinoza 2023</p>
                     </div>
                 </div>
             </footer>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,15 @@
 //import '../CSS/Home.css'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useSpring, animated } from '@react-spring/web'
+import 'animate.css';
+
+
 import ReactFlagsSelect from "react-flags-select";
 import gif from '../assets/bonfire.gif'
-
-
-import Linkspagina from '../Componentes/Linkspagina';
 import PiePagina from '../Componentes/PiePagina';
 
 import AboutES from "../Componentes/AboutES"
@@ -30,244 +31,227 @@ import ExperienciaPL from '../Componentes/ExperienciaPL';
 function Home() {
     const [selectedIdioma, setSelectedIdioma] = useState('ES');
     const countries = ["GB", "DE", "ES", "PL",];
+    const [springs, api] = useSpring(() => ({
+        from: { x: 0 },
+        config: { duration: 500 },
+        onRest: () => setAnimating(false),
+    }));
+
+    const [animating, setAnimating] = useState(true);
+
+    useEffect(() => {
+        if (animating) {
+            api.start({
+                from: {
+                    x: -500,
+                },
+                to: {
+                    x: 0,
+                },
+            });
+        }
+    }, [animating, api]);
+
+
     return (
         <>
+            <div className="hero">
+                <div className="cube"></div>
+                <div className="cube"></div>
+                <div className="cube"></div>
+                <div className="cube"></div>
+                <div className="cube"></div>
+                <div className="cube"></div>
 
-            <ToastContainer
-                autoClose={1000} />
 
-            <div>
-                <section id='language-btn' className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12" >
-                   
+
+
+                <ToastContainer
+                    autoClose={1000} />
+
+                <div>
+                    <section id='language-btn' className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12" >
+
                         <ReactFlagsSelect
                             selected={selectedIdioma}
                             onSelect={code => setSelectedIdioma(code)}
                             countries={countries}
                             customLabels={{ GB: "English", DE: "Deutsch", ES: "Español", PL: "Polski" }}
                         />
-                   
-                    {/* <div className="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6" >
+
+                        {/* <div className="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6" >
                         <Linkspagina idioma={"ES"}/>
                     </div> */}
-                </section>
+                    </section>
 
-               
+
+                    <section id='language' className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6" >
+                        {selectedIdioma == 'ES' && <div>
+                            <h1 style={{ color: "white" }}>Hola! Soy Sebastian</h1>
+                            <h1 style={{ color: "white" }}>Bienvenido a mi portafolio</h1>
+                        </div>
+                        }
+
+                        {selectedIdioma == 'GB' && <div>
+                            <h1 style={{ color: "white" }}>Hello! Im Sebastian</h1>
+                            <h1 style={{ color: "white" }}>Welcome to my web page</h1>
+                        </div>
+                        }
+
+                        {selectedIdioma == 'PL' && <div>
+                            <h1 style={{ color: "white" }}>Cześć, jestem Sebastian</h1>
+                            <h1 style={{ color: "white" }}>Witam na mojej stronie internetowej</h1>
+                        </div>
+                        }
+                        {selectedIdioma == 'DE' && <div>
+                            <h1 style={{ color: "white" }}>Hallo, mein Name ist Sebastian</h1>
+                            <h1 style={{ color: "white" }}>Willkommen auf meiner Website</h1>
+                        </div>
+                        }
+                    </section>
+
+                </div>
+
+
+
+                <animated.div id='about-me' style={{ ...springs, }}>
+                    <img id='bonfire' src={gif} alt="Bonfire!!..." />
+                    <section className='mt-5 mb-5 border border-white col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6'>
+                        {selectedIdioma == 'ES' && <div className="card">
+                            <AboutES></AboutES>
+                        </div>
+                        }
+
+                        {selectedIdioma == 'GB' && <div className="card">
+                            <div className="card-body" style={{ color: "white" }}>
+                                <AboutGB></AboutGB>
+                            </div>
+                        </div>
+                        }
+
+                        {selectedIdioma == 'PL' && <div className="card">
+                            <div className="card-body" style={{ color: "white" }}>
+                                <AboutPL></AboutPL>
+                            </div>
+                        </div>
+                        }
+
+                        {selectedIdioma == 'DE' && <div className="card">
+                            <div className="card-body" style={{ color: "white" }}>
+                                <AboutDE></AboutDE>
+                            </div>
+                        </div>
+                        }
+                    </section>
+                </animated.div>
 
                 <section id='language' className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6" >
+
                     {selectedIdioma == 'ES' && <div>
-                        <h1 style={{ color: "white" }}>Hola! Soy Sebastian</h1>
-                        <h1 style={{ color: "white" }}>Bienvenido a mi portafolio</h1>
+                        <h3 style={{ color: "white" }}>Mis Proyectos</h3>
+
                     </div>
                     }
 
                     {selectedIdioma == 'GB' && <div>
-                        <h1 style={{ color: "white" }}>Hello! Im Sebastian</h1>
-                        <h1 style={{ color: "white" }}>Welcome to my web page</h1>
+                        <h3 style={{ color: "white" }}>My projects</h3>
+
                     </div>
                     }
 
                     {selectedIdioma == 'PL' && <div>
-                        <h1 style={{ color: "white" }}>Cześć, jestem Sebastian</h1>
-                        <h1 style={{ color: "white" }}>Witam na mojej stronie internetowej</h1>
+                        <h3 style={{ color: "white" }}>Moje projekty</h3>
+
                     </div>
                     }
                     {selectedIdioma == 'DE' && <div>
-                        <h1 style={{ color: "white" }}>Hallo, mein Name ist Sebastian</h1>
-                        <h1 style={{ color: "white" }}>Willkommen auf meiner Website</h1>
+                        <h3 style={{ color: "white" }}>Meine Projekte</h3>
+
                     </div>
                     }
                 </section>
 
+                <div id='about-me-Pro'>
+                    <section className='mt-5 mb-5 border border-white col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6'>
+                        {selectedIdioma == 'ES' &&
+                            <ProyectoRuletaES />
+                        }
+
+                        {selectedIdioma == 'PL' && <div className="card">
+                            <div className="card-body" style={{ color: "white" }}>
+                                <ProyectoRuletaPL></ProyectoRuletaPL>
+                            </div>
+                        </div>
+                        }
+
+                        {selectedIdioma == 'GB' && <div className="card">
+                            <div className="card-body" style={{ color: "white" }}>
+                                <ProyectoRuletaGB></ProyectoRuletaGB>
+                            </div>
+                        </div>
+                        }
+
+                        {selectedIdioma == 'DE' && <div className="card">
+                            <div className="card-body" style={{ color: "white" }}>
+                                <ProyectoRuletaDE></ProyectoRuletaDE>
+                            </div>
+                        </div>
+                        }
+                    </section>
+                </div>
+
+                <section id='language' className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6" >
+                    {selectedIdioma == 'ES' && <div>
+                        <h3 style={{ color: "white" }}>Mis Experiencias Laborales</h3>
+                    </div>
+                    }
+
+                    {selectedIdioma == 'GB' && <div>
+                        <h3 style={{ color: "white" }}>My Working Experiences </h3>
+                    </div>
+                    }
+
+                    {selectedIdioma == 'PL' && <div>
+                        <h3 style={{ color: "white" }}>Moje doświadczenia zawodowe</h3>
+                    </div>
+                    }
+                    {selectedIdioma == 'DE' && <div>
+                        <h3 style={{ color: "white" }}>Meine Berufserfahrungen</h3>
+                    </div>
+                    }
+                </section>
+
+                <div id='about-me-Exp'>
+                    <section className='mt-5 mb-5 border border-white col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6'>
+                        {selectedIdioma == 'ES' &&
+                            <ExperienciaES />
+                        }
+
+                        {selectedIdioma == 'PL' && <div className="card">
+                            <div className="card-body" style={{ color: "white" }}>
+                                <ExperienciaPL />
+                            </div>
+                        </div>
+                        }
+
+                        {selectedIdioma == 'GB' && <div className="card">
+                            <div className="card-body" style={{ color: "white" }}>
+                                <ExperienciaGB />
+                            </div>
+                        </div>
+                        }
+
+                        {selectedIdioma == 'DE' && <div className="card">
+                            <div className="card-body" style={{ color: "white" }}>
+                                <ExperienciaDE />
+                            </div>
+                        </div>
+                        }
+                    </section>
+                </div>
+
+                <PiePagina />
             </div>
-
-            
-
-            <div id='about-me'>
-                <img id='bonfire' src={gif} alt="Bonfire!!..." />
-                <section className='mt-5 mb-5 border border-white col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6'>
-                    {selectedIdioma == 'ES' && <div className="card">
-                        <AboutES></AboutES>
-                    </div>
-                    }
-
-                    {selectedIdioma == 'GB' && <div className="card">
-                        <div className="card-body" style={{ color: "white" }}>
-                            <AboutGB></AboutGB>
-                        </div>
-                    </div>
-                    }
-
-                    {selectedIdioma == 'PL' && <div className="card">
-                        <div className="card-body" style={{ color: "white" }}>
-                            <AboutPL></AboutPL>
-                        </div>
-                    </div>
-                    }
-
-                    {selectedIdioma == 'DE' && <div className="card">
-                        <div className="card-body" style={{ color: "white" }}>
-                            <AboutDE></AboutDE>
-                        </div>
-                    </div>
-                    }
-                </section>
-            </div>
-
-            <section id='language' className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6" >
-                {selectedIdioma == 'ES' && <div>
-                    <h3 style={{ color: "white" }}>Mis Proyectos</h3>
-                    
-                </div>
-                }
-
-                {selectedIdioma == 'GB' && <div>
-                    <h3 style={{ color: "white" }}>My projects</h3>
-                    
-                </div>
-                }
-
-                {selectedIdioma == 'PL' && <div>
-                    <h3 style={{ color: "white" }}>Moje projekty</h3>
-                    
-                </div>
-                }
-                {selectedIdioma == 'DE' && <div>
-                    <h3 style={{ color: "white" }}>Meine Projekte</h3>
-                    
-                </div>
-                }
-            </section>
-
-            <div id='about-me-Pro'>
-                <section className='mt-5 mb-5 border border-white col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6'>
-                    {selectedIdioma == 'ES' &&
-                        <ProyectoRuletaES />
-                    }
-
-                    {selectedIdioma == 'PL' && <div className="card">
-                        <div className="card-body" style={{ color: "white" }}>
-                            <ProyectoRuletaPL></ProyectoRuletaPL>
-                        </div>
-                    </div>
-                    }
-
-                    {selectedIdioma == 'GB' && <div className="card">
-                        <div className="card-body" style={{ color: "white" }}>
-                            <ProyectoRuletaGB></ProyectoRuletaGB>
-                        </div>
-                    </div>
-                    }
-
-                    {selectedIdioma == 'DE' && <div className="card">
-                        <div className="card-body" style={{ color: "white" }}>
-                            <ProyectoRuletaDE></ProyectoRuletaDE>
-                        </div>
-                    </div>
-                    }
-                </section>
-            </div>
-
-            <section id='language' className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6" >
-                {selectedIdioma == 'ES' && <div>
-                    <h3 style={{ color: "white" }}>Mis Experiencias Laborales</h3>
-                </div>
-                }
-
-                {selectedIdioma == 'GB' && <div>
-                    <h3 style={{ color: "white" }}>My Working Experiences </h3>
-                </div>
-                }
-
-                {selectedIdioma == 'PL' && <div>
-                    <h3 style={{ color: "white" }}>Moje doświadczenia zawodowe</h3>
-                </div>
-                }
-                {selectedIdioma == 'DE' && <div>
-                    <h3 style={{ color: "white" }}>Meine Berufserfahrungen</h3>
-                </div>
-                }
-            </section>
-
-            <div id='about-me-Exp'>
-                <section className='mt-5 mb-5 border border-white col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6'>
-                    {selectedIdioma == 'ES' &&
-                        <ExperienciaES />
-                    }
-
-                    {selectedIdioma == 'PL' && <div className="card">
-                        <div className="card-body" style={{ color: "white" }}>
-                            <ExperienciaPL />
-                        </div>
-                    </div>
-                    }
-
-                    {selectedIdioma == 'GB' && <div className="card">
-                        <div className="card-body" style={{ color: "white" }}>
-                            <ExperienciaGB />
-                        </div>
-                    </div>
-                    }
-
-                    {selectedIdioma == 'DE' && <div className="card">
-                        <div className="card-body" style={{ color: "white" }}>
-                            <ExperienciaDE />
-                        </div>
-                    </div>
-                    }
-                </section>
-            </div>
-
-            {/* <section id='language' className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6" >
-                {selectedIdioma == 'ES' && <div>
-                    <h3 style={{ color: "white" }}>Contacto</h3>
-                </div>
-                }
-
-                {selectedIdioma == 'GB' && <div>
-                    <h3 style={{ color: "white" }}>Contact me! </h3>
-                </div>
-                }
-
-                {selectedIdioma == 'PL' && <div>
-                    <h3 style={{ color: "white" }}>gdzie się ze mną skontaktować</h3>
-                </div>
-                }
-                {selectedIdioma == 'DE' && <div>
-                    <h3 style={{ color: "white" }}>wo Sie mich kontaktieren können</h3>
-                </div>
-                }
-            </section>
-
-            <div id='about-me'>
-                <section className='mt-5 mb-5 border border-white col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6'>
-                    {selectedIdioma == 'ES' &&
-                        <ExperienciaES />
-                    }
-
-                    {selectedIdioma == 'PL' && <div className="card">
-                        <div className="card-body" style={{ color: "white" }}>
-                            <ExperienciaPL />
-                        </div>
-                    </div>
-                    }
-
-                    {selectedIdioma == 'GB' && <div className="card">
-                        <div className="card-body" style={{ color: "white" }}>
-                            <ExperienciaGB />
-                        </div>
-                    </div>
-                    }
-
-                    {selectedIdioma == 'DE' && <div className="card">
-                        <div className="card-body" style={{ color: "white" }}>
-                            <ExperienciaDE />
-                        </div>
-                    </div>
-                    }
-                </section>
-            </div> */}
-
-            <PiePagina/>
         </>
     )
 }
